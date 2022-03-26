670a62a0de954b4a0f86c3786407478bf9dbe6de
David-Lor <17401854+David-Lor@users.noreply.github.com>
2022-03-26 16:45:47 +0100

    feat: environment variable support on go code; clean unused code

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ RUN go build -o /tmp/gobuilt
 # TODO Use scratch image
 FROM alpine:3.6
 
+ENV SOCKS_SERVER_HOST="0.0.0.0" \
+    SOCKS_SERVER_PORT="9050"
+
 COPY --from=build /tmp/gobuilt /socks-proxy-balancer
 RUN chmod +x /socks-proxy-balancer
 ENTRYPOINT ["/socks-proxy-balancer"]
--- src/main.go
+++ src/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
-	"strconv"
-	"strings"
+	"os"
 	"sync"
 )
 
@@ -50,7 +48,6 @@ func getLoadBalancer() *LoadBalancer {
 /*
 	Joins the local and remote connections together
 */
-
 func pipeConnections(localConn, remoteConn net.Conn) {
 	//goland:noinspection GoUnhandledErrorResult
 	go func() {
@@ -93,168 +90,59 @@ func handleTunnelConnection(conn net.Conn) {
 }
 
 /*
-	Calls the apprpriate handle_connections based on tunnel mode
-*/
-func handleConnection(conn net.Conn, tunnel bool) {
-	if tunnel {
-		handleTunnelConnection(conn)
-	} else if address, err := handleSocksConnection(conn); err == nil {
-		serverResponse(conn, address)
-	}
-}
-
-/*
-	Detect the addresses which can  be used for dispatching in non-tunnelling mode.
-	Alternate to ipconfig/ifconfig
-*/
-func detectInterfaces() {
-	fmt.Println("--- Listing the available adresses for dispatching")
-	ifaces, _ := net.Interfaces()
-
-	for _, iface := range ifaces {
-		if (iface.Flags&net.FlagUp == net.FlagUp) && (iface.Flags&net.FlagLoopback != net.FlagLoopback) {
-			addrs, _ := iface.Addrs()
-			for _, addr := range addrs {
-				if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-					if ipnet.IP.To4() != nil {
-						fmt.Printf("[+] %s, IPv4:%s\n", iface.Name, ipnet.IP.String())
-					}
-				}
-			}
-		}
-	}
-
-}
-
-/*
-	Gets the interface associated with the IP
-*/
-func getIfaceFromIp(ip string) string {
-	ifaces, _ := net.Interfaces()
-
-	for _, iface := range ifaces {
-		if (iface.Flags&net.FlagUp == net.FlagUp) && (iface.Flags&net.FlagLoopback != net.FlagLoopback) {
-			addrs, _ := iface.Addrs()
-			for _, addr := range addrs {
-				if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-					if ipnet.IP.To4() != nil {
-						if ipnet.IP.String() == ip {
-							return iface.Name + "\x00"
-						}
-					}
-				}
-			}
-		}
-	}
-	return ""
-}
-
-/*
 	Parses the command line arguements to obtain the list of load balancers
 */
-func parseLoadBalancers(args []string, tunnel bool) {
-	if len(args) == 0 {
-		log.Fatal("[FATAL] Please specify one or more load balancers")
-	}
+func parseLoadBalancers(proxies []HostPort) {
+	lbList = make([]LoadBalancer, len(proxies))
 
-	lbList = make([]LoadBalancer, flag.NArg())
-
-	for idx, a := range args {
-		splitted := strings.Split(a, "@")
-		iface := ""
-		var lbIp string
-		var lbPort int
-		var err error
-
-		if tunnel {
-			ipPort := strings.Split(splitted[0], ":")
-			if len(ipPort) != 2 {
-				log.Fatal("[FATAL] Invalid address specification ", splitted[0])
-				return
-			}
-
-			lbIp = ipPort[0]
-			lbPort, err = strconv.Atoi(ipPort[1])
-			if err != nil || lbPort <= 0 || lbPort > 65535 {
-				log.Fatal("[FATAL] Invalid port ", splitted[0])
-				return
-			}
-
-		} else {
-			lbIp = splitted[0]
-			lbPort = 0
-		}
-
-		if net.ParseIP(lbIp).To4() == nil {
-			log.Fatal("[FATAL] Invalid address ", lbIp)
-		}
-
+	for i, proxy := range proxies {
 		contRatio := 1
-		if len(splitted) > 1 {
-			contRatio, err = strconv.Atoi(splitted[1])
-			if err != nil || contRatio <= 0 {
-				log.Fatal("[FATAL] Invalid contention ratio for ", lbIp)
-			}
+		log.Printf("[INFO] Load balancer %d: %s:%d, contention ratio: %d\n", i+1, proxy.Host, proxy.Port, contRatio)
+		lbList[i] = LoadBalancer{
+			address:            proxy.ToAddr(),
+			iface:              "",
+			contentionRatio:    contRatio,
+			currentConnections: 0,
 		}
-
-		// Obtaining the interface name of the load balancer IP's doesn't make sense in tunnel mode
-		if !tunnel {
-			iface = getIfaceFromIp(lbIp)
-			if iface == "" {
-				log.Fatal("[FATAL] IP address not associated with an interface ", lbIp)
-			}
-		}
-
-		log.Printf("[INFO] Load balancer %d: %s, contention ratio: %d\n", idx+1, lbIp, contRatio)
-		lbList[idx] = LoadBalancer{address: fmt.Sprintf("%s:%d", lbIp, lbPort), iface: iface, contentionRatio: contRatio, currentConnections: 0}
 	}
 }
 
 /*
 	Main function
 */
-func main() {
-	var lhost = flag.String("lhost", "127.0.0.1", "The host to listen for SOCKS connection")
-	var lport = flag.Int("lport", 8080, "The local port to listen for SOCKS connection")
-	var detect = flag.Bool("list", false, "Shows the available addresses for dispatching (non-tunnelling mode only)")
-	var tunnel = flag.Bool("tunnel", false, "Use tunnelling mode (acts as a transparent load balancing proxy)")
-
-	flag.Parse()
-	if *detect {
-		detectInterfaces()
-		return
-	}
-
+func mainLogic(settings *Settings) {
 	// Disable timestamp in log messages
 	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
 
-	// Check for valid IP
-	if net.ParseIP(*lhost).To4() == nil {
-		log.Fatal("[FATAL] Invalid host ", *lhost)
-	}
-
-	// Check for valid port
-	if *lport < 1 || *lport > 65535 {
-		log.Fatal("[FATAL] Invalid port ", *lport)
-	}
-
-	//Parse remaining string to get addresses of load balancers
-	parseLoadBalancers(flag.Args(), *tunnel)
-
-	localBindAddress := fmt.Sprintf("%s:%d", *lhost, *lport)
+	// Parse load balancers from settings into LoadBalancer objects
+	parseLoadBalancers(settings.ProxiesConnect)
 
 	// Start local server
+	localBindAddress := settings.ToAddr()
 	l, err := net.Listen("tcp4", localBindAddress)
 	if err != nil {
-		log.Fatalln("[FATAL] Could not start local server on ", localBindAddress)
+		log.Fatalln("[FATAL] Could not start local server on", localBindAddress)
 	}
-	log.Println("[INFO] Local server started on ", localBindAddress)
+	log.Println("[INFO] Local server started on", localBindAddress)
 	//goland:noinspection GoUnhandledErrorResult
 	defer l.Close()
 
 	mutex = &sync.Mutex{}
 	for {
 		conn, _ := l.Accept()
-		go handleConnection(conn, *tunnel)
+		go handleTunnelConnection(conn)
 	}
+}
+
+func main() {
+	settings, errs := LoadSettings()
+	if len(errs) > 0 {
+		fmt.Println("Errors in settings:")
+		for _, err := range errs {
+			fmt.Printf("\t- %s\n", err.Error())
+		}
+		os.Exit(1)
+	}
+
+	mainLogic(settings)
 }
